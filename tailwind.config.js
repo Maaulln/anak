59782f79e4bdc3f6cca9c1f4d525aeb6ea3a823a
Oktavian's Mac <oktaviansmac@192.168.1.3>
2025-07-02 08:46:04 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/*module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}*/
+
+// tailwind.config.js using Vite
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+ 
