422ae820a352fc5b1441642beea8305d18a1814e
Moh Minas Sahiddin <minassahiddin@gmail.com>
2025-07-03 22:47:01 +0700

    cover untuk loading awal masuk

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
+import { useState, useEffect } from "react";
 import ROUTES from "./routes/index.jsx";
+import Cover from "./pages/Cover.jsx";
 // import Register from "./pages/Register";
 // import Dashboard from "./pages/Dashboard";
 // import ProtectedRoute from "./routes/ProtectedRoute";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading time
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); // 2 seconds
+    // Clear the timer if the component unmounts
+    return () => clearTimeout(timer);
+  }, []);
+  if(loading){
+    return <Cover />;
+  }
   return (
     <Routes>
       <Route path="/" element={<Navigate to={ROUTES.LOGIN} />} />
