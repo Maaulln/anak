283661c7e3dffe2e9ba38c05b83ba765f8438e85
Oktavian's Mac <oktaviansmac@192.168.1.3>
2025-07-03 13:54:12 +0700

    Solve tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,22 @@
-// src/App.jsx
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
-import ROUTES from "./routes";
+import ROUTES from "./routes/index.jsx";
 // import Register from "./pages/Register";
 // import Dashboard from "./pages/Dashboard";
 // import ProtectedRoute from "./routes/ProtectedRoute";
 
-// export default function App() {
-//   return (
-//     <Routes>
-//       <Route path={ROUTES.LOGIN} element={<Login />} />
-//       {/* <Route path="/register" element={<Register />} /> */}
-//       {/* <Route path="/dashboard" element={
-//           <ProtectedRoute>
-//             <Dashboard />
-//           </ProtectedRoute>
-//         } 
-//       /> */}
-//     </Routes>
-//   );
-// }
 export default function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-yellow-100">
-      <h1 className="text-red-600 text-2xl font-bold rounded-lg bg-red p-6 shadow">
-        Tailwind is working 🎉
-      </h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigate to={ROUTES.LOGIN} />} />
+      <Route path={ROUTES.LOGIN} element={<Login />} />
+      {/* <Route path="/register" element={<Register />} /> */}
+      {/* <Route path="/dashboard" element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        } 
+      /> */}
+    </Routes>
   );
 }
